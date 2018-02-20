39325e53fd83fff2549d0b54f4404a2f24409fe1
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-20 19:46:29 +0000

    Minor update on source file

--- LinkedLists.playground/Sources/List.swift
+++ LinkedLists.playground/Sources/List.swift
@@ -5,16 +5,16 @@
 
 import Foundation
 
-class List<T> {
+public class List<T> {
   
-  var value: T
-  var nextItem: List<T>?
+  public var value: T
+  public var nextItem: List<T>?
   
-  convenience init?(_ values: T...) {
+  public convenience init?(_ values: T...) {
     self.init(Array(values))
   }
   
-  init?(_ values: [T]) {
+  public init?(_ values: [T]) {
     guard let first = values.first else {
       return nil
     }
