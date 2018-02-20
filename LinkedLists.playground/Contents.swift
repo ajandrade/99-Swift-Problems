5e77ab34168bcaa9a987acdca00e9c6a2536a5e7
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-20 21:49:44 +0000

    Added solution for P02

--- LinkedLists.playground/Contents.swift
+++ LinkedLists.playground/Contents.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-/*
- P01 (*) Find the last element of a linked list.
+/* P01 (*) Find the last element of a linked list.
  http://www.enekoalonso.com/projects/99-swift-problems/#p01
  
  Example:
@@ -23,5 +22,32 @@ extension List {
   }
 }
 
-let p01 = List(1, 1, 2, 3, 5, 8)
-p01?.last
+// Test P01:
+//let p01 = List(1, 1, 2, 3, 5, 8)
+//p01?.last
+
+/* P02 (*) Find the last but one element of a linked list.
+ http://www.enekoalonso.com/projects/99-swift-problems/#p02
+ 
+ Example:
+  List(1, 1, 2, 3, 5, 8).pennultimate
+ 
+ Result:
+  5
+*/
+
+extension List {
+  var pennultimate: T? {
+    return pennultimate(self, previousValue: nil)
+  }
+  
+  private func pennultimate(_ list: List, previousValue: T?) -> T? {
+    guard let nextItem = list.nextItem else { return previousValue }
+    return pennultimate(nextItem, previousValue: list.value)
+  }
+}
+
+// Test P02:
+let p02 = List(1, 1, 2, 3, 5, 8)
+p02?.pennultimate
+
