1ad15f885f0ce45abbccd6729eec97f055aae5d9
Amadeu Andrade <amadeu.dev@gmail.com>
2018-02-20 19:46:41 +0000

    Added solution for P01

--- LinkedLists.playground/Contents.swift
+++ LinkedLists.playground/Contents.swift
@@ -10,3 +10,18 @@ import Foundation
  Result:
   8
 */
+
+extension List {
+  
+  var last: T {
+    return last(self)
+  }
+  
+  private func last(_ list: List) -> T {
+    guard let nextItem = list.nextItem else { return list.value }
+    return last(nextItem)
+  }
+}
+
+let p01 = List(1, 1, 2, 3, 5, 8)
+p01?.last
