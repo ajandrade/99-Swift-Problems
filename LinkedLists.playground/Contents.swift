aa9cba93bbf713e2c0b13ce0850bd70f79ef804d
Amadeu Andrade <aandrade@blissapplications.com>
2018-02-21 13:54:42 +0000

    Added solution for P03

--- LinkedLists.playground/Contents.swift
+++ LinkedLists.playground/Contents.swift
@@ -48,6 +48,34 @@ extension List {
 }
 
 // Test P02:
-let p02 = List(1, 1, 2, 3, 5, 8)
-p02?.pennultimate
+//let p02 = List(1, 1, 2, 3, 5, 8)
+//p02?.pennultimate
+
+/* P03 (*) Find the Kth element of a linked list.
+ http://www.enekoalonso.com/projects/99-swift-problems/#p03
+ By convention, the first element in the list is element 0. Use Swift subscripts to get the value from the linked list.
+
+Example:
+ let list = List(1, 1, 2, 3, 5, 8)
+ list[2]
+
+Result:
+ 2
+*/
+
+extension List {
+    subscript(index: Int) -> T? {
+        return element(self, at: index)
+    }
+    
+    private func element(_ list: List, at index: Int) -> T? {
+        if index == 0 { return list.value }
+        guard let next = list.nextItem else { return nil }
+        return element(next, at: index - 1)
+    }
+}
+
+// Test P03:
+let list = List(1, 1, 2, 3, 5, 8)!
+list[6]
 
